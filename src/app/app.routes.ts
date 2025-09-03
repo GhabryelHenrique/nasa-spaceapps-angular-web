b7fb854caaa08082a842e869cdc84237b11a59dc
GhabryelHenrique <ghabryelhenrique@hotmail.com>
2025-09-03 12:02:49 -0300

    [Auto] Update teams and Events

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { TeamsComponent } from './teams/teams.component';
 import { WarRoomComponent } from './war-room/war-room.component';
+import { ChallengesComponent } from './challenges/challenges.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
+  { path: 'desafios', component: ChallengesComponent },
   { path: 'times', component: TeamsComponent },
   { path: 'sala-de-guerra', component: WarRoomComponent },
   { path: 'politica-privacidade', component: PrivacyPolicyComponent },
