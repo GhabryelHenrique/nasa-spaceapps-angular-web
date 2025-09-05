52d168576d054ab703163d83f96d3240c12995d0
Ghabryel Henrique <ghabryelhenrique@hotmail.com>
2025-09-05 17:45:41 -0300

    [Auto] Update teams and Events

--- src/app/landing-page/components/events-map/events-map.component.ts
+++ src/app/landing-page/components/events-map/events-map.component.ts
@@ -33,11 +33,11 @@ export class EventsMapComponent implements OnInit {
       title: 'MTI',
       info: 'Polo de capacitação dos mentores do NASA Space Apps Challenge'
     },
-    // {
-    //   position: { lat: -18.956634, lng: -48.271558 },
-    //   title: 'Unitri',
-    //   info: 'Local de apoio para networking e atividades complementares do NASA Space Apps Challenge'
-    // },
+    {
+      position: { lat: -18.956634, lng: -48.271558 },
+      title: 'Una',
+      info: 'Local de apoio para networking e atividades complementares do NASA Space Apps Challenge'
+    },
     {
       position: { lat: -18.879712, lng: -48.246336 },
       title: 'Brain',
@@ -60,7 +60,7 @@ export class EventsMapComponent implements OnInit {
     },
     {
       position: { lat: -18.950453, lng: -48.257845 },
-      title: 'Mackense',
+      title: 'Colégio Ann Mackenzie',
       info: 'Local de apoio para networking e atividades complementares do NASA Space Apps Challenge'
     },
     {
