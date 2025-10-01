824e2c6c5f55ab3f336464030ac266cbf7c7def1
Ghabryel Henrique <ghabryelhenrique@hotmail.com>
2025-10-01 10:38:29 -0300

    [Auto] Update teams and Events

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: false,
   googleMapsApiKey: 'AIzaSyDUHGGWwcM5VZJc2NqGoukmP4F7z7Uu6A8',
   apiUrl: 'https://api.nasaspaceappsuberlandia.com',
+  // apiUrl: 'http://localhost:3000',
   apiVersion: 'v1'
 };
